0466f190031555c1767d89240375dcb28e9d8a70
Adam <you@example.com>
2020-12-11 14:37:53 +0100

    cmake

---
+++ cmake-build-debug/CMakeFiles/JNP_1_task_4.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aryski/3SEM/JNP/JNP-1-task-4/computer_example.cc" "/Users/aryski/3SEM/JNP/JNP-1-task-4/cmake-build-debug/CMakeFiles/JNP_1_task_4.dir/computer_example.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/JNP_1_task_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JNP_1_task_4.dir/computer_example.cc.o"
+  "JNP_1_task_4"
+  "JNP_1_task_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JNP_1_task_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
